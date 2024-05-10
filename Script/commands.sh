81c528ec4f38be60fe9739b415071fa3fbaa3200
Deepika <95289716+Deepika-r-naik@users.noreply.github.com>
2024-05-10 12:30:22 +0530

    Create commands.sh

---
+++ Script/commands.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Uptime:"
+uptime
+
+echo "Kernel information:"
+uname -a
+
+echo "Active Directory information (if applicable):"
+adinfo
+
+echo "Disk space usage:"
+df -PTH
+
+echo "Network interface information (using ifconfig):"
+ifconfig
+
+echo "Network interface information (using ip a):"
+ip a
+
+echo "Routing table:"
+route -n
+
+echo "System DMI information:"
+dmidecode -t1
+
+echo "Distribution information:"
+cat /etc/*-release
